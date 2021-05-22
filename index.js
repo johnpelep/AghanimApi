5328a2ebbb17c2bd234503d27090b8ad81b3e2ba
Glitch (silver-pewter-atrociraptor) <none>
2021-05-22 13:04:26 +0000

    Import of johnpelep/AghanimApi

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const { port } = require('./config');
+const players = require('./routes/players');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.use('/players', players);
+
+app.listen(port, () => {
+  console.log(`AghanimApi listening at port:${port}`);
+});
