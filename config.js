d0f753843d552fc3588c4913852caadadbf0025d
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-05-22 17:03:11 +0800

    added endpoint for hindaw

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   port: process.env.PORT,
+  key: process.env.STEAM_API_KEY,
+  getPlayerSummaryUrl: process.env.STEAM_API_URL,
+  getMatchesUrl: process.env.RECENT_MATCHES_URL,
+  resolveVanityUrl: process.env.RESOLVE_VANITY_URL,
   connectionString: process.env.CONNECTION_STRING,
 };
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const app = express();
 const { port } = require('./config');
+const accountService = require('./services/accountService');
+const accountHelper = require('./helpers/accountHelper');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  res.send('Hello worlds!');
+});
+
+// hindaw
+app.get('/player', async (req, res) => {
+  const personaName = req.query.personaName;
+
+  // get account from db
+  let account = await accountService.getAccount({ personaName: personaName });
+
+  // check if account exist
+  if (!account)
+    return res.send(
+      `account **${personaName}** is wara sa listahan. Paki-add anay gamit an **Invite!** command`
+    );
+
+  // sync account
+  account = await accountHelper.syncAccount(account);
+
+  // check if account has record
+  if (!account.record || !account.record.streakCount)
+    return res.send(
+      `account **${personaName}** has no match recorded for this month`
+    );
+
+  res.send(account);
+});
+
+app.post('/', (req, res) => {
   res.send('Hello worlds!');
 });
 
