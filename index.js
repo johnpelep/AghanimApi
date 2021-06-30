f80bb318088e63632c25f556166c59fa41cbf898
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-06-30 14:11:39 +0800

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { port } = require('./config');
 const players = require('./routes/players');
 const constants = require('./routes/constants');
 const interactions = require('./routes/interactions');
 
+app.use(cors());
 app.use(maybe(express.json()));
 app.use(maybe(express.urlencoded({ extended: true })));
 
